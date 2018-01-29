f5ed9117b8a994865bc0db66bdedcfa928fed4ea
Stephen Mathews <busysteve@gmail.com>
2018-01-29 07:07:23 -0500

    Update dataview.cpp

--- ann_testing/dataview.cpp
+++ ann_testing/dataview.cpp
@@ -7,10 +7,12 @@ int main( int argc, char **argv )
 	int count = atoi(argv[2]);
 	int min   = atoi(argv[3]);
 	int max   = atoi(argv[4]);
-	char* on_c  = argv[5];
-	char* off_c = argv[6];
-	char* datafile  = argv[7];
-	char* labelfile = argv[8];
+    float mul = atof(argv[5]);
+	char* on_c  = argv[6];
+	char* off_c = argv[7];
+	char* datafile  = argv[8];
+	char* labelfile = argv[9];
+	int newline = atoi(argv[10]);
 
 	unsigned char stash[28][28];
 
@@ -34,20 +36,62 @@ int main( int argc, char **argv )
 		{
 			for( int j=0; j < 28; j++ )
 			{
-				if( stash[i][j] > min && stash[i][j] < max )
-					printf("%s", on_c);
-				else
-					printf("%s", off_c);
+                if( mul > 0.0 )
+                {
+				    printf("%1.3f ", ((float)stash[i][j] * mul) );
+                }
+                else
+                {
+				    if( stash[i][j] > min && stash[i][j] < max )
+					    printf("%s", on_c);
+				    else
+					    printf("%s", off_c);
+                }
 			}
-			printf("\n");
+			if( newline > 0 ) printf("\n");
+			//else printf(" ");
 		}
 
 		fread( &stash[0][0], 1, 1, labels );
+
+		switch( stash[0][0] )
+		{
+			case 0:
+				printf("1 0 0 0 0 0 0 0 0 0 ");
+				break;
+			case 1:
+				printf("0 1 0 0 0 0 0 0 0 0 ");
+				break;
+			case 2:
+				printf("0 0 1 0 0 0 0 0 0 0 ");
+				break;
+			case 3:
+				printf("0 0 0 1 0 0 0 0 0 0 ");
+				break;
+			case 4:
+				printf("0 0 0 0 1 0 0 0 0 0 ");
+				break;
+			case 5:
+				printf("0 0 0 0 0 1 0 0 0 0 ");
+				break;
+			case 6:
+				printf("0 0 0 0 0 0 1 0 0 0 ");
+				break;
+			case 7:
+				printf("0 0 0 0 0 0 0 1 0 0 ");
+				break;
+			case 8:
+				printf("0 0 0 0 0 0 0 0 1 0 ");
+				break;
+			case 9:
+				printf("0 0 0 0 0 0 0 0 0 1 ");
+				break;
+		}
+
 		printf( "%d\n", stash[0][0] );
 	}
 
 	return 0;
 }
-
 
 
